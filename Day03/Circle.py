733e92a2bf6b6a470d4b0bf83ec06792ae86e76b
NoamWAc <noamwac@gmail.com>
2025-04-14 19:22:43 +0300

    Modified rectange and circle to use arguments rather than input

--- Day03/Circle.py
+++ Day03/Circle.py
@@ -1,9 +1,16 @@
 # circle.py
 
 import math
+import sys
 
 # Ask the user for the radius
-radius = float(input("Enter the radius of the circle: "))
+##radius = float(input("Enter the radius of the circle: "))
+radius = sys.argv[1]
+try:
+    radius = float(radius) # Convert the input to a float
+except ValueError:
+    print("Invalid input. Please enter a numeric value for the radius.")
+    sys.exit(1)
 
 # Calculate area and circumference
 area = math.pi * radius ** 2
--- Day03/rectangle.py
+++ Day03/rectangle.py
@@ -1,8 +1,18 @@
 # rectangle.py
+import sys
 
 # Ask the user for height and width
-height = float(input("Enter the height of the rectangle: "))
-width = float(input("Enter the width of the rectangle: "))
+#height = float(input("Enter the height of the rectangle: "))
+#width = float(input("Enter the width of the rectangle: "))
+
+height = sys.argv[1]
+width = sys.argv[2]
+try:
+    height = float(height) # Convert the input to a float
+    width = float(width) # Convert the input to a float
+except ValueError:
+    print("Invalid input. Please enter numeric values for height and width.")
+    sys.exit(1)
 
 # Calculate area and perimeter
 area = height * width
