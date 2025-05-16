31934308a22d8715bb80eaa49df2c3143dc5ff0e
NoamWAc <noamwac@gmail.com>
2025-05-16 17:49:29 +0300

    number guessing games 1-6 added

---
+++ Day04/number_guessing_game_3.py
@@ -0,0 +1,30 @@
+import random
+
+#generates a random number from 1 to 20
+game_num = random.randrange(1,20) 
+guessed_num = "0"
+
+#while loop to keep asking for a guess until the user guesses correctly
+while int(guessed_num) != game_num:
+    #asks user for their guess and converts input to int
+    guessed_num = input("Enter your guess of which integer between 1-20 was generated: "); 
+    if guessed_num == "x":
+        print("Thanks for playing!")
+        exit (0)
+    guessed_num = int(guessed_num)
+    #if the guess is correct, it will inform them and then exit the program
+    if  guessed_num == game_num:
+        print("You guessed correctly! The number really was ", guessed_num, "!")
+        exit ("Thanks for playing!")
+
+    elif guessed_num > game_num:
+        print("Your guess was too high. Give it another try!")
+        
+    else:
+        #if the guess is not correct, it will check if it is too high or too low
+        #and print the appropriate message
+        print("Your guess was too low. Give it another try!")
+
+    
+    
+
