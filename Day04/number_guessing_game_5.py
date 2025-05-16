d9822885af10b143dc0d54756f046c6b275c15f7
NoamWAc <noamwac@gmail.com>
2025-05-16 23:15:10 +0300

    levels 5 and 6

--- Day04/number_guessing_game_5.py
+++ Day04/number_guessing_game_5.py
@@ -9,6 +9,10 @@ def non_digit_input():
     #if the input is x it will exit the program, if s it will show the number
     #if m it will toggle move mode, if d it will toggle debug mode
     #if m, d or irrelevant input, it will restart the loop
+    global game_num
+    global guessed_num
+    global debug_is_on
+    global move_mode_is_on
     if guessed_num == "x":
         print("Thanks for playing!")
         exit (0)
@@ -17,27 +21,30 @@ def non_digit_input():
         return "keep going"
     elif guessed_num == "m":
         move_mode_is_on = not move_mode_is_on
-        print("Move mode is now", "on. The random number might change \
-            by up to 2 after each guess!" if move_mode_is_on \
-            else "off. The random number will not change anymore.")    
+        print("Move mode is now", "on. The game's number might increase or decrease by up to 2 after each guess!" if move_mode_is_on \
+            else "off. The number will not change anymore.")    
     elif guessed_num == "d":
         # Toggle debug mode
         debug_is_on = not debug_is_on
-        print("Debug mode is now "+ "on." if debug_is_on else "off.")
+        print("Debug mode is now "+ ("on." if debug_is_on else "off."))
     else:
         return "restart the loop"
+
 def move():
     # Move mode: change the number by up to 2
+    global game_num
     game_num = game_num + random.randint(-2, 2)
     if game_num < 1:
         game_num += 4
     elif game_num > 20:
         game_num -= 4
     print("The number has been moved by up to 2.")
-
+    return "keep going"
 
 #while loop to keep asking for a guess until the user guesses correctly
 while guessed_num != int(game_num):
+    if move_mode_is_on and str(guessed_num).isdigit():
+            move()
     #if debug mode is on, it will print the number
     if debug_is_on:
         print("The number is " + str(game_num))
@@ -53,23 +60,22 @@ while guessed_num != int(game_num):
         # if not, exits the program
         if  guessed_num == game_num:
             print("You guessed correctly, the number was " + str(game_num) + "!")
-            if input(print("Do you want to play again? (enter 'yes' if you do): ")) == "y":
+            if input(print("Do you want to play again? (enter 'yes' if you do): ")) == "yes":
                 game_num = random.randrange(1,20)
                 guessed_num = ""
                 continue
             else:
                 exit("Thanks for playing!")
-        
+        elif guessed_num < 1 or guessed_num > 20:            
+            #if the input isn't between 1 and 20, it will inform the user and ask for another guess
+            print("Please only enter a number between 1 and 20!")
+            continue
         #if the guess is too high, it will inform them and ask for another guess
         elif guessed_num > game_num:
             print("Your guess was too high. Give it another try!")
-            
+            continue
         elif guessed_num < game_num:
-            #if the guess is not correct, it will check if it is too high or too low
-            #and print the appropriate message
+        #if the guess is too low, it will inform them and ask for another guess
             print("Your guess was too low. Give it another try!")
-        elif guessed_num < 1 or guessed_num > 20:            
-            #if the input is between 1 and 20, it will inform the user and ask for another guess
-            print("Please enter a number between 1 and 20.")
-        if move_mode_is_on:
-            move()
+            continue
+        
--- Day04/number_guessing_game_6.py
+++ Day04/number_guessing_game_6.py
@@ -1,22 +1,104 @@
 import random
 
-#generates a random number from 1 to 20
-game_num = random.randrange(1,20) 
 
-#asks user for their guess and converts input to int
-guessed_num = int(input("Enter your guess of which integer between 1-20 was generated! ")); 
 
-if  guessed_num == game_num:
-    print("You guessed correctly! The number really was ", guessed_num, "!")
-elif guessed_num > game_num:
-    print("Your guess was too high!")
-else:
-    #if the guess is not correct, it will check if it is too high or too low
-    #and print the appropriate message
-    #it will also print the number that was generated
-    #so the user can see what they were guessing against
-    #and then exit the program
-    print("Your guess was too low!")
-    print("The number was ", game_num)
-    print("Thanks for playing!")
-    exit (0);
+global game_num
+global guessed_num
+global debug_is_on
+global move_mode_is_on
+
+game_num = random.randrange(1,20)
+guessed_num = ""
+debug_is_on=False
+move_mode_is_on=False
+
+def initialize_game():
+    #resets settings and generates a new random number between 1 to 20
+    global game_num
+    global guessed_num
+    global debug_is_on
+    global move_mode_is_on
+    game_num = random.randrange(1,20)
+    guessed_num = ""
+    debug_is_on=False
+    move_mode_is_on=False
+
+
+def non_digit_input_check():
+    #if the input is x it will exit the program, if s it will show the number
+    #if m it will toggle move mode, if d it will toggle debug mode
+    #if m, d or irrelevant input, it will restart the loop
+    global game_num
+    global guessed_num
+    global debug_is_on
+    global move_mode_is_on
+    if guessed_num == "x":
+        print("Thanks for playing!")
+        exit (0)
+    elif guessed_num == "n":
+        initialize_game()
+        print("The game has been reset.")
+        return "restart the loop"
+    elif guessed_num == "s" and not debug_is_on:
+        print("The number is " + str(game_num))
+        return "keep going"
+    elif guessed_num == "m":
+        move_mode_is_on = not move_mode_is_on
+        print("Move mode is now", "on. The game's number might increase or decrease by up to 2 after each guess!" if move_mode_is_on \
+            else "off. The number will not change anymore.")    
+    elif guessed_num == "d":
+        # Toggle debug mode
+        debug_is_on = not debug_is_on
+        print("Debug mode is now "+ ("on." if debug_is_on else "off."))
+    else:
+        return "restart the loop"
+
+def move():
+    # Move mode: change the number by up to 2
+    global game_num
+    game_num = game_num + random.randint(-2, 2)
+    if game_num < 1:
+        game_num += 4
+    elif game_num > 20:
+        game_num -= 4
+    print("The number may have increased or decreased by up to 2.")
+    return "keep going"
+
+#while loop to keep asking for a guess until the user guesses correctly
+while guessed_num != int(game_num):
+    if move_mode_is_on and str(guessed_num).isdigit():
+            move()
+    #if debug mode is on, it will print the number
+    if debug_is_on:
+        print("The number is currently " + str(game_num))
+    guessed_num = input("Enter your guess of which integer between 1-20 was generated: "); 
+    
+    if guessed_num.isdigit() == False:
+            if non_digit_input_check()=="restart the loop":
+                continue
+
+    else:        
+        guessed_num = int(guessed_num)
+        #if the guess is correct, it will inform them and then ask if to contiue
+        # if not, exits the program
+        if  guessed_num == game_num:
+            print("You guessed correctly, the number was " + str(game_num) + "!")
+            if input(print("Do you want to play again? (enter 'yes' if you do): ")) == "yes":
+                game_num = random.randrange(1,20)
+                guessed_num = ""
+                continue
+            else:
+                exit("Thanks for playing!")
+        elif guessed_num < 1 or guessed_num > 20:            
+            #if the input isn't between 1 and 20, it will inform the user and ask for another guess
+            print("Please only enter a number between 1 and 20!")
+            continue
+        #if the guess is too high, it will inform them and ask for another guess
+        elif guessed_num > game_num:
+            print("Your guess was too high. Give it another try!")
+            continue
+        elif guessed_num < game_num:
+        #if the guess is too low, it will inform them and ask for another guess
+            print("Your guess was too low. Give it another try!")
+            continue
+        
