39cfa68b69217d2c43906a52027dd605fe337358
NoamWAc <noamwac@gmail.com>
2025-05-26 23:14:05 +0300

    Added explanations for count_words_in_lists.py

--- Day05/count_words_in_lists.py
+++ Day05/count_words_in_lists.py
@@ -1,8 +1,12 @@
 celestial_objects = [
     'Moon', 'Gas', 'Asteroid', 'Dwarf', 'Asteroid', 'Moon', 'Asteroid'
 ]
+
 words = []
 count = []
+# This code counts the occurrences of words (strings) in a list and prints the count for each unique object.
+# It initializes two lists: one for unique words and another for their counts.
+
 for word in celestial_objects:
     if word not in words:
         words.append(word)
@@ -10,5 +14,6 @@ for word in celestial_objects:
     else:
         count[words.index(word)] += 1
 
+# Print the unique words and their counts
 for i in range(len(words)):
     print(f"{words[i]} {count[i]}")
