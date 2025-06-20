421dab2d3c9205ec2cdf4dadb78b8021cba3fdb4
Noam Weil <noam.weil@weizmann.ac.il>
2025-06-21 00:17:59 +0300

    Trying doctest

--- Day09/my-python-project/src/initialize.py
+++ Day09/my-python-project/src/initialize.py
@@ -6,12 +6,15 @@ def initialize_game():
     """
     This function initializes the game by setting up the necessary configurations and generating a new game number.
     It also ensures that the config module is available and sets default values if it doesn't exist.
-    >>> initialize_game()
-    returns a new game number between 1 and 20
+    >>> initialize_game() # doctest: +SKIP
+    Returns a random number between 1 and 20
     """
     
     reset_config.reset_config() # Tries to reset config file debug mode and move mode to false. If the file doesn't exist, create a default one
     game_num = rng_1_to_20.generate_new_num()
     return game_num
 
-
+if __name__ == "__main__":
+    # This block is only executed when the script is run directly, not when imported as a module.
+    import doctest
+    doctest.testmod(verbose=True)
