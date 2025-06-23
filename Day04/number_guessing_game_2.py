3230ce96ddc7b99fecfb3c72398316f3494c97f4
Noam Weil <noam.weil@weizmann.ac.il>
2025-06-23 12:56:48 +0300

    Pre-changes, after advice from brother

--- Day04/number_guessing_game_2.py
+++ Day04/number_guessing_game_2.py
@@ -1,11 +1,11 @@
 import random
 
 #generates a random number from 1 to 20
-game_num = random.randrange(1,20) 
+target_num = random.randrange(1,20) 
 guessed_num = "0"
 
 #while loop to keep asking for a guess until the user guesses correctly
-while int(guessed_num) != game_num:
+while int(guessed_num) != target_num:
     #asks user for their guess and converts input to int
     guessed_num = input("Enter your guess of which integer between 1-20 was generated: "); 
     if guessed_num == "x":
@@ -13,12 +13,12 @@ while int(guessed_num) != game_num:
         exit (0)
     guessed_num = int(guessed_num)
     #if the guess is correct, it will inform them and then exit the program
-    if  guessed_num == game_num:
+    if  guessed_num == target_num:
         print("You guessed correctly! The number really was ", guessed_num, "!")
         print("Thanks for playing!")
         exit (0)
 
-    elif guessed_num > game_num:
+    elif guessed_num > target_num:
         print("Your guess was too high. Give it another try!")
         
     else:
