3c5fc51faa03fe3ade890ee2abdc40df197fad9b
Noam Weil <noam.weil@weizmann.ac.il>
2025-06-24 21:14:58 +0300

    removed redundant module

--- Day09/my-python-project/src/main.py
+++ Day09/my-python-project/src/main.py
@@ -1,9 +1,31 @@
-# main.py
+import base_functions
+import process_input
+import process_input
 
-import modular_doctest_number_guessing_game_6 as game
 
-def main():
-    game.interactive_number_guessing_game()
+def interactive_number_guessing_game():
+    """
+    This function runs an interactive number guessing game where the user has to guess a randomly-generated number between 1 and 20.
+    """
+    game_num = base_functions.initialize_game()
+    user_input = ""
+    
+    while True:  
+        #if debug mode is on, it will print the number
+        if base_functions.CONFIG["DEBUG_MODE_IS_ON"]:
+            print("The number is currently " + str(game_num))
+        
+        user_input = input("Enter your guess of which integer between 1-20 was generated: ")
+        
+        if user_input.isdigit() == False: # if the input is not a digit, it will check if it is a special command
+            game_num = process_input.non_digit_input_check(user_input, game_num)
+            continue   
+        else:
+            game_num = process_input.digit_input_check(int(user_input), game_num) # if the input is a digit, it will process it
+            continue
+    raise Exception ("An error occurred. To avoid infinite loop, please troubleshoot or try again.") # if the loop is not working properly, it will raise an exception to avoid infinite loop
+
+
 
 if __name__ == "__main__":
-    main()
+    interactive_number_guessing_game()
